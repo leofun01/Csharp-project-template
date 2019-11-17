d0229fcaa9b2264e5a12556980f2fcae89f9999d
leofun01 <leofun01@gmail.com>
2019-11-17 20:43:36 +0200

    Create `AssemblyInfo.cs`

---
+++ CsProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CsProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CsProject")]
+[assembly: AssemblyCopyright("Copyright © author YYYY-YYYY")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(true)]
+
+[assembly: Guid("xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx3")]
+
+[assembly: AssemblyVersion("0.1.0.1")]
+[assembly: AssemblyFileVersion("0.1.0.1")]
---
+++ CsProject/Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("Tests for CsProject.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright © author YYYY-YYYY")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx4")]
+
+[assembly: AssemblyVersion("0.1.0.1")]
+[assembly: AssemblyFileVersion("0.1.0.1")]
